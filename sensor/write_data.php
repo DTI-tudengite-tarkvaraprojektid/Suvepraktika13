a1985d12344ff6c8cb820a19de797298c67aac32
Katri Palo <32160266+katripalo@users.noreply.github.com>
2018-06-12 13:55:56 +0300

    Update write_data.php

--- sensor/write_data.php
+++ sensor/write_data.php
@@ -1,22 +1,24 @@
 <?php
 
     // Prepare variables for database connection
-   
-    $dbusername = "if17";  // enter database username, I used "arduino" in step 2.2
-    $dbpassword = "if17";  // enter database password, I used "arduinotest" in step 2.2
-    $server = "localhost"; // IMPORTANT: if you are using XAMPP enter "localhost", but if you have an online website enter its address, ie."www.yourwebsite.com"
+    $host='sql203.epizy.com';
+    $dbusername = "epiz_22225394";  // enter database username, I used "arduino" in step 2.2
+    $dbpassword = "ryhm13";  // enter database password, I used "arduinotest" in step 2.2
+    $server = "suvepraktika.epizy.com"; // IMPORTANT: if you are using XAMPP enter "localhost", but if you have an online website enter its address, ie."www.yourwebsite.com"
 
     // Connect to your database
 
-    $dbconnect = mysql_pconnect($server, $dbusername, $dbpassword);
-    $dbselect = mysql_select_db("if17_ryhm13",$dbconnect);
-
+    $dbconnect = mysql_connect($host, $dbusername, $dbpassword);
+    $dbselect = mysql_select_db("epiz_22225394_suvepraktika13",$dbconnect);
+   echo "yhendatud";
+    echo mysql_error();
     // Prepare the SQL statement
 
-    $sql = "INSERT INTO if17_ryhm13.sensor (value) VALUES ('".$_GET["value"]."')";    
+    $sql = "INSERT INTO epiz_22225394_suvepraktika13.sensor (value) VALUES ('".$_GET["value"]."')";    
 
     // Execute SQL statement
 
     mysql_query($sql);
-
+  echo "saadetud";
+    echo mysql_error();
 ?>
