ef07c57793284bdeaa7c904391e69977b5fbb1c6
desulg <31885461+desulg@users.noreply.github.com>
2018-06-12 09:23:50 +0300

    Heli, temp, niiskus, valgus..mingil määral heli ka

--- NodeMcu.ino
+++ NodeMcu.ino
@@ -1,17 +1,21 @@
-
 #include <ESP8266WiFi.h>
 #include <dht.h>
 #include "DHT.h"
+#include <Wire.h>
+#include <BH1750.h>
+
 #define dht_apin D0 // Analog Pin sensor is connected to
- 
+
+BH1750 lightMeter;
 dht DHT;
   
   const char* ssid     = "TLU";
   const char* password = "";     
   
   int wifiStatus;
-  
-  void setup() {
+
+/*-------------------------------------SETUP STARTS HERE BELOW----------------------------*/  
+void setup() {
   
   Serial.begin(74880);
   delay(200);
@@ -31,10 +35,14 @@ dht DHT;
     delay(500);
     Serial.print(".");
   }
+    Wire.begin();
+
+    lightMeter.begin();  
   
-  }   
-  
-  void loop() {
+ }
+ 
+/*-------------------------------------LOOP STARTS HERE BELOW----------------------------*/
+void loop() {
   wifiStatus = WiFi.status();
   
   if(wifiStatus == WL_CONNECTED){
@@ -51,7 +59,8 @@ dht DHT;
     float db;
     int val;
     val=analogRead(A0); 
-    db = 20 * log(val/3.3);
+    float voltage = val * (5.0/1023);
+    db = 20 * log10(voltage/0.005012);
     Serial.println(db);
     //
 /*-------------------------TEMP----------------------*/
@@ -64,9 +73,13 @@ dht DHT;
     Serial.print(DHT.temperature); 
     Serial.println("C  ");
 
+/*-------------------------VALGUS----------------------*/
 
+  uint16_t lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
 
- 
   delay(1000); // check for connection every once a second
   
   }
