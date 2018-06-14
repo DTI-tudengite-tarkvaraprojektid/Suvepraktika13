cf1c298c749a360f99c9c9832ad86fa27c9af3f4
Katri Palo <32160266+katripalo@users.noreply.github.com>
2018-06-14 14:37:00 +0300

    Update write_data.php

--- sensor/write_data.php
+++ sensor/write_data.php
@@ -1,24 +1,31 @@
 <?php
-
+    date_default_timezone_set('Europe/Tallinn');
     // Prepare variables for database connection
-    $host='sql203.epizy.com';
-    $dbusername = "epiz_22225394";  // enter database username, I used "arduino" in step 2.2
+    $host='localhost';
+    $dbusername = "id6141049_suvepraktika";  // enter database username, I used "arduino" in step 2.2
     $dbpassword = "ryhm13";  // enter database password, I used "arduinotest" in step 2.2
-    $server = "suvepraktika.epizy.com"; // IMPORTANT: if you are using XAMPP enter "localhost", but if you have an online website enter its address, ie."www.yourwebsite.com"
-
-    // Connect to your database
-
-    $dbconnect = mysql_connect($host, $dbusername, $dbpassword);
-    $dbselect = mysql_select_db("epiz_22225394_suvepraktika13",$dbconnect);
-   echo "yhendatud";
-    echo mysql_error();
-    // Prepare the SQL statement
-
-    $sql = "INSERT INTO epiz_22225394_suvepraktika13.sensor (value) VALUES ('".$_GET["value"]."')";    
+    $server = "sisekliima.000webhostapp.com"; // IMPORTANT: if you are using XAMPP enter "localhost", but if you have an online website enter its address, ie."www.yourwebsite.com"
+    $database = "id6141049_sensor";
+    $date = new DateTime();
+    $date = $date->format('Y-m-d H:i:s') . "\n";
+    echo $date;
+   
 
     // Execute SQL statement
+    $ruum = 457;
+    $temperatuur = $_REQUEST["temperatuur"];
+    $ohuniiskus = $_REQUEST["ohuniiskus"];
+    $valgus = $_REQUEST["valgus"];
 
-    mysql_query($sql);
-  echo "saadetud";
-    echo mysql_error();
-?>
+    
+    //mysql_query($sql);
+    $mysqli = new mysqli($GLOBALS['host'], $GLOBALS['dbusername'], $GLOBALS['dbpassword'], $GLOBALS['database']);
+		$stmt = $mysqli->prepare("INSERT INTO sensor(id, time, ruum, temperatuur, ohuniiskus, valgus) VALUES (DEFAULT, ?, ?, ?, ?, ?)");
+		//$result = $mysqli->query($stmt);
+		$stmt->bind_param("siddi", $date, $ruum, $temperatuur, $ohuniiskus, $valgus);
+		$stmt->execute();
+    echo "saadetud";
+	echo $mysqli->error;
+	$stmt->close();
+	$mysqli->close();
+?>	
